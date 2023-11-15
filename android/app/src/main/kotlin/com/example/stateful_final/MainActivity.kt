3e4146f6aff28e5169445439fca3c5c3f8be2144
icodingchef <icodingchef@gmail.com>
2023-11-15 11:25:21 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stateful_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stateful_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'my_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('widget property'),
+      ),
+      body: const Center(
+        child: MyPage(
+          passedColor: Colors.green,
+        ),
+      ),
+    );
+  }
+}
